321c7d4469b3e7a34fb50dca2e19e0c4f4d4dea6
WilkinsonK <keenanwilkinson@outlook.com>
2023-04-01 19:07:44 -0500

    add requirements and actions.sh

---
+++ actions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Simple directives for building subsequent
+# python packages.
+
+set -eu -o pipefail
+
+# Builds package distributable.
+build() {
+    python setup.py sdist bdist_wheel
+}
+
+# Install package in local virtual environment.
+local() {
+    find dist/ -type f -iname "*.whl" | xargs python -m pip install
+}
+
+publish() {
+    python -m twine dist/*
+}
+
+for command in "$@"
+do
+    $command
+done
